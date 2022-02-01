7bec27ac4f0f8ae777230d7b50bd841bb33fc328
Joyce de Fatima <joycecosta453@hotmail.com>
2022-02-01 16:55:32 -0300

    first commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.css";
+
+function Button(props: any) {
+  return <button type="submit" onClick={props.handleClick}>Botão</button>
+}
+
+export default Button
